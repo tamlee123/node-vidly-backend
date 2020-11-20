b35f459f2755aaa90e062d1fdd427a604acebf81
tam <cherykim123@gmail.com>
2020-11-19 18:48:30 -0800

    get, post, put, delete from an array in our backend app to mongodb

---
+++ index.js
@@ -0,0 +1,61 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Comedy" },
+  { id: 3, name: "Thriller" },
+];
+
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+app.post("/api/genres", (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.send(error);
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("This genre with the id was not found");
+  const { error } = validateGenre(req.body);
+  if (error) return res.send(error);
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+app.delete("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("This genre with the id was not found");
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+  res.send(genre);
+});
+
+function validateGenre(genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(genre);
+}
+
+app.get("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("the genre with the id was not found");
+  res.send(genre);
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}`));
