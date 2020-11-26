81b5c912f751c78a313c7821c6ef3a4ff3a81de2
tam <cherykim123@gmail.com>
2020-11-25 22:25:21 -0800

    correct building movie api and apply embedding

--- routes/movies.js
+++ routes/movies.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
  
-    const movies = await Movie.find().sort('name');
+    const movies = await Movie.find().sort('title');
   
     res.send(movies);
   });
@@ -39,7 +39,7 @@ router.get("/", async (req, res) => {
     if (error) return res.status(400).send(error.details[0].message);
   
     const movie = await Movie.findByIdAndUpdate(req.params.id,
-       {title: req.body.name, 
+       {title: req.body.title, 
         genre: {
             _id: genre._id,
             name: genre.name
@@ -58,7 +58,7 @@ router.get("/", async (req, res) => {
   
   router.delete("/:id", async  (req, res) => {
   
-    const movie = await Customer.findByIdAndRemove(req.params.id);
+    const movie = await Movie.findByIdAndRemove(req.params.id);
   
     if (!movie)
       return res.status(404).send("The movie with the given ID was not found.");
@@ -67,7 +67,7 @@ router.get("/", async (req, res) => {
   
   router.get("/:id", async (req, res) => {
   
-    const movie = await Customer.findById(req.params.id)
+    const movie = await Movie.findById(req.params.id)
     
     if (!movie)
       return res.status(404).send("The movie with the given ID was not found.");
