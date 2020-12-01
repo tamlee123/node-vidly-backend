62fdd00f14d3c4ef1cc615b5047413b5126ca404
tam <cherykim123@gmail.com>
2020-11-30 21:29:59 -0800

    validating objectId

--- models/rental.js
+++ models/rental.js
@@ -1,5 +1,5 @@
 const Joi = require("joi");
-
+ Joi.objectId = require('joi-objectid') (Joi);
 const mongoose = require('mongoose');
 
 
@@ -61,8 +61,8 @@ const Rental = mongoose.model('Rental', new mongoose.Schema ({
 
 function validateRental(rental) {
     const schema = {
-      customerId: Joi.string().required(),
-      movieId: Joi.string().required(),
+      customerId: Joi.objectId().required(),
+      movieId: Joi.objectId().required(),
     };
   
     return Joi.validate(rental, schema);
