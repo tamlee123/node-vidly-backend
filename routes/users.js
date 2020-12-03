77b778ef93e397ca181b79f89cf964c04638baea
tam <cherykim123@gmail.com>
2020-12-02 20:56:21 -0800

    hashing  password  using  bcrypt  genSalt  and  hash

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
 const {User, validate} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require("express");
@@ -11,15 +13,12 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({email: req.body.email});
     if (user) return res.status(400).send('User already resgistered');
   
-    user = new User({ 
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-      
-    });
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
     await user.save();
     
-    res.send(user);
+    res.send(_.pick(user, ['_id','name', 'email']));
   });
 
   module.exports = router; 
