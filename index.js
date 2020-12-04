91fcefd54b6fa29e250ddd56733a0e1fed2f1913
tam <cherykim123@gmail.com>
2020-12-03 19:23:09 -0800

    generate authentication tokens and store secrets in the environment variables

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require ('config');
 const Joi = require("joi");
 Joi.objectId = require('joi-objectid') (Joi);
 const mongoose = require('mongoose');
@@ -7,7 +8,13 @@ const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth")
 const app = express();
+
+if(!config.get('jwtPrivateKey')){
+  console.error('FATAL ERROR: jwtPrivateKey is not defined');
+  process.exit(1);
+}
 
 mongoose.connect('mongodb://localhost/vidly2', {useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...'))
@@ -16,10 +23,10 @@ mongoose.connect('mongodb://localhost/vidly2', {useNewUrlParser: true, useUnifie
 app.use(express.json());
 
 app.use("/api/genres", genres);
-app.use("/api/customers", customers);
+app.use("/api/customers", customers);                                                                                                                                                                                                                                                          
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
-
+app.use("/api/auth", auth);
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,6 @@
+const config = require ('config');
+const jwt = require ('jsonwebtoken');
+const Joi = require("joi");
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const {User} = require('../models/user');
@@ -5,7 +8,7 @@ const mongoose = require('mongoose');
 const express = require("express");
 const router = express.Router();
 
-
+                             
 router.post('/', async (req, res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
@@ -15,8 +18,9 @@ router.post('/', async (req, res) => {
   
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password.');
-    
-    res.send(true);
+
+    const token = jwt.sign({_id: user._id}, config.get('jwtPrivateKey'));// payload and secret key to verify signature
+    res.send(token);
 });
 
 
