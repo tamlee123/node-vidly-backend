8a863302438e40cb6f7013b2e9d93b7128e5bef5
tam <cherykim123@gmail.com>
2020-12-04 20:31:59 -0800

    get the infor of the current login user

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const auth = require('../middleware/auth');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const {User, validate} = require('../models/user');
@@ -6,6 +7,10 @@ const express = require("express");
 const router = express.Router();
 
 
+router.get('/me', auth, async(req,res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
 router.post('/', async (req, res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
