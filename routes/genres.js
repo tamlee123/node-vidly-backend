0ccc4fb4a17e5b896389a3422de8520e4d2bcd3e
tam <cherykim123@gmail.com>
2020-12-07 19:54:07 -0800

    handling rejected promises, error 500 (internal server error) could not connected mongodb

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const admin = require('../middleware/admin');
 const auth = require('../middleware/auth');
 const {Genre, validate} = require('../models/genre');
 const mongoose = require('mongoose');
@@ -5,10 +6,14 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
- 
-  const genres = await Genre.find().sort('name');
-
-  res.send(genres);
+  try{
+    const genres = await Genre.find().sort('name');
+    res.send(genres);
+  }
+  catch(ex){
+    res.status(500).send('something failed');
+  }
+  
 });
 
 router.post("/", auth, async (req, res) => {
@@ -39,7 +44,7 @@ router.put("/:id", async (req, res) => {
   res.send(genre);
 });
 
-router.delete("/:id", async  (req, res) => {
+router.delete("/:id",[auth, admin], async  (req, res) => {
 
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
