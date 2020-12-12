18c73e71bba3213fb315f56e5a2b3c34e590f414
tam <cherykim123@gmail.com>
2020-12-11 21:57:35 -0800

    extracting routes from index to a private file to make index more neat

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 require('express-async-errors');
 require('winston-mongodb');
 const winston = require('winston');
-const error = require('./middleware/error');
 const config = require ('config');
 const Joi = require("joi");
 Joi.objectId = require('joi-objectid') (Joi);
 const mongoose = require('mongoose');
 const express = require("express");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
+require('./startup/routes')(app);
 const app = express();
 
 winston.handleExceptions(
@@ -33,8 +27,7 @@ process.on('unhandledRejection', (ex)=>{
 //    winston.add (new winston.transports.File({ filename: 'logfile.log' }))
 //   ]
 // });
-const p = Promise.reject(new Error('Something failed misarably!'));
-p.then(() => console.log('Done'));
+
 
 if(!config.get('jwtPrivateKey')){
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
@@ -45,15 +38,7 @@ mongoose.connect('mongodb://localhost/vidly2', {useNewUrlParser: true, useUnifie
   .then(() => console.log('Connected to MongoDB...'))
   .catch(() => console.error('Could not connect to MongoDB...'));
 
-app.use(express.json());
 
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);                                                                                                                                                                                                                                                          
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
