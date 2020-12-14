be4e3591199b8f8aed6ad1659d05c87aec533d27
tam <cherykim123@gmail.com>
2020-12-13 16:07:42 -0800

    extracting the logging logic

--- index.js
+++ index.js
@@ -1,44 +1,18 @@
-require('express-async-errors');
-require('winston-mongodb');
-const winston = require('winston');
 const config = require ('config');
 const Joi = require("joi");
 Joi.objectId = require('joi-objectid') (Joi);
-const mongoose = require('mongoose');
 const express = require("express");
-require('./startup/routes')(app);
 const app = express();
 
-winston.handleExceptions(
-  new winston.transports.File({ filename: 'uncaughtExceptions.log'})
-);
-
-process.on('unhandledRejection', (ex)=>{
-  throw ex;
-});
-
- winston.add (winston.transports.File, {filename: 'logfile.log'});
- winston.add (winston.transports.MongoDB, {
-   db: 'mongodb://localhost/vidly2',
-   level: 'info'
-  });
-//  const logger = winston.createLogger({
-//   transports: [
-//    winston.add (new winston.transports.File({ filename: 'logfile.log' }))
-//   ]
-// });
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
 
 
 if(!config.get('jwtPrivateKey')){
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
   process.exit(1);
 }
-
-mongoose.connect('mongodb://localhost/vidly2', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(() => console.error('Could not connect to MongoDB...'));
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ startup/logging.js
@@ -0,0 +1,24 @@
+const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+
+module.exports = function() {
+    winston.handleExceptions(
+        new winston.transports.File({ filename: 'uncaughtExceptions.log'})
+      );
+      
+      process.on('unhandledRejection', (ex)=>{
+        throw ex;
+      });
+      
+       winston.add (winston.transports.File, {filename: 'logfile.log'});
+       winston.add (winston.transports.MongoDB, {
+         db: 'mongodb://localhost/vidly2',
+         level: 'info'
+        });
+      //  const logger = winston.createLogger({
+      //   transports: [
+      //    winston.add (new winston.transports.File({ filename: 'logfile.log' }))
+      //   ]
+      // });
+}
