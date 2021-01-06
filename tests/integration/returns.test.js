ebdca54f0bda3e148b474dd24c47ed61d7a1db51
tam <cherykim123@gmail.com>
2021-01-05 23:15:41 -0800

    testing response-return the rental if input is valid

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -135,4 +135,13 @@ describe('/api/returns', () => {
         const movieInDb = await Movie.findById(movieId);
         expect(movieInDb.numberInStock).toBe(movie.numberInStock + 1); // or toBe(11)
     });
+
+    it('Should return the rental if input is valid', async () => {
+
+        const res = await exec();
+        const rentalInDb = await Rental.findById(rental._id);
+        expect(Object.keys(res.body)).toEqual(
+            expect.arrayContaining(['dateOut', 'dateReturn', 'rentalFee', 'customer', 'movie'])
+        );
+    });
 });
