c57af2b854c16b82d38116fad66a65fa897d984b
tam <cherykim123@gmail.com>
2021-01-05 21:36:10 -0800

    testing the movie stock

--- routes/returns.js
+++ routes/returns.js
@@ -2,6 +2,7 @@ const moment = require ('moment');
 const express = require("express");
 const auth = require("../middleware/auth");
 const { Rental } = require("../models/rental");
+const { Movie } = require('../models/movie');
 const router = express.Router();
 
 
@@ -22,11 +23,14 @@ router.post("/",auth, async(req, res) => {
    if(rental.dateReturn) return res.status(400).send('Rental already processed.');
 
    rental.dateReturn = new Date();
-   const rentalDays = moment().diff(rental.dateOUt, 'days');
+   const rentalDays = moment().diff(rental.dateOut, 'days');
    rental.rentalFee = rentalDays *  rental.movie.dailyRentalRate;
    
    await rental.save();
 
+   await Movie.update({_id: rental.movie._id}, {
+      $inc: {numberInStock: 1}
+   });
    return res.status(200).send();
 
 });
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,8 +1,10 @@
 const moment = require('moment');
 const request = require('supertest');
 const {Rental} = require ('../../models/rental');
+const {Movie} = require('../../models/movie');
 const {User} = require ('../../models/user');
 const mongoose = require ('mongoose');
+
 
 
 describe('/api/returns', () => {
@@ -10,6 +12,7 @@ describe('/api/returns', () => {
     let customerId;
     let movieId;
     let rental;
+    let movie;
     let token;
 
     const exec = () => {
@@ -25,6 +28,15 @@ describe('/api/returns', () => {
         movieId = mongoose.Types.ObjectId();
         
         token = new User().generateAuthToken();
+
+        movie = new Movie({
+            _id: movieId,
+            title: '12345',
+            dailyRentalRate: 2,
+            genre: {name: '12345'},
+            numberInStock: 10
+        });
+        await movie.save();
 
         rental = new Rental({
             customer: {
@@ -45,6 +57,7 @@ describe('/api/returns', () => {
     afterEach(async () => { 
         await server.close();
         await Rental.remove({});
+        await Movie.remove({});
     });
 
     
@@ -113,6 +126,13 @@ describe('/api/returns', () => {
 
         const res = await exec();
         const rentalInDb = await Rental.findById(rental._id);
-        expect(rentalInDb.rentalFee).toBeDefined(); //14 dollars
+        expect(rentalInDb.rentalFee).toBe(14); 
+    });
+
+    it('Should increase the movie stock if input is valid', async () => {
+
+        const res = await exec();
+        const movieInDb = await Movie.findById(movieId);
+        expect(movieInDb.numberInStock).toBe(movie.numberInStock + 1); // or toBe(11)
     });
 });
