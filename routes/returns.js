20ccce0826afb08abf62e24ab0d654f55951fb9b
tam <cherykim123@gmail.com>
2021-01-06 16:34:03 -0800

    refracturing new Date and calculating rentalFee

--- routes/returns.js
+++ routes/returns.js
@@ -1,5 +1,4 @@
 const Joi = require ('joi');
-const moment = require ('moment');
 const validate = require('../middleware/validate');
 const { Rental } = require('../models/rental');
 const { Movie } = require('../models/movie');
@@ -17,16 +16,16 @@ router.post('/',[auth, validate(validateReturn)], async(req, res) => {
 
    if(rental.dateReturn) return res.status(400).send('Rental already processed.');
 
-   rental.dateReturn = new Date();
-   const rentalDays = moment().diff(rental.dateOut, 'days');
-   rental.rentalFee = rentalDays *  rental.movie.dailyRentalRate;
+   rental.return()
    
    await rental.save();
 
    await Movie.update({_id: rental.movie._id}, {
       $inc: {numberInStock: 1}
    });
-   return res.status(200).send(rental);
+   //return res.status(200).send(rental);
+   //express will set 200 by default so we dont need it
+   return res.send(rental);
 
 });
 
